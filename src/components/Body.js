d0e2034d7ce6f406fe108f6b3b0b22e79f1f4322
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-10-20 23:58:08 -0300

    feat

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import Title from './Title';
-import MissionCard from './MissionCard';
-import missions from '../data/missions';
+import Bio from './bio'
 import styles from './styles.module.scss';
 
-class Body extends React.Component {
-  render() {
-    return (
-      <body>
-        <div>
-          <a href="#" className={styles.menu}> </a>
-        </div >
-        <div data-testid="missions">
-          <Title headline="Missões" />
-          <div className={styles.mission}>
-            {missions.map((mission) => (<MissionCard
-              key={mission.name}
-              name={`Nome: ${mission.name}`}
-              year={`Ano: ${mission.year}`}
-              country={`País: ${mission.country}`}
-              destination={`Destino: ${mission.destination}`}
-            />))}
-          </div>
-        </div>
-      </body>
-    );
-  }
+function Body() {
+  return (
+    <section className={styles.content}>
+      <div className={styles.bio_show}>
+        <p>
+          Olá, obrigado pela visita! Me chamo Victor, e estou mergulhando nesse universo codificado!
+        </p>
+      </div>
+      <div className={styles.bio}>
+        {<Bio />}
+      </div>
+    </section>
+  );
 }
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,28 +8,53 @@ class Header extends React.Component {
       <header>
         <div className={styles.header}>
           <div className={styles.sides}>
-            <html>
-              <a href="#down" className={styles.logo}>PORTIFOLIO</a>
-            </html>
-          </div>
-          <div className={styles.sides}>
           </div>
           <div className={styles.info}>
-            <h4>Saudações! Seja bem-vindo(a) ao portifolio</h4>
+            <h4>Saudações! Seja bem-vindo(a) ao meu portifolio</h4>
             <h1>VICTOR BARIATTO REKSIDLER</h1>
             <div className={styles.meta}>
-              <a target="_b" className={styles.author}></a>
-              Full Stack Web Developer
+              <div className={styles.author} />
+              <h4>Full Stack Web Developer</h4>
+            </div>
+            <div>
+              <a href="#down" className={styles.meet}>Clique aqui para me conhecer melhor</a>
             </div>
           </div>
         </div>
+        <div className={styles.links2}>
+          <div className={styles.icons}>
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original-wordmark.svg" title="React" alt="React" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/redux/redux-original.svg" title="Redux" alt="Redux " width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain-wordmark.svg" title="CSS3" alt="CSS" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg" title="HTML5" alt="HTML" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mysql/mysql-original-wordmark.svg" title="MySQL" alt="MySQL" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original-wordmark.svg" title="NodeJS" alt="NodeJS" width="40" height="40" />&nbsp;
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sass/sass-original.svg" title="Sass" alt="Sass" width="40" height="40" />
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg" title="TypeScript" alt="TypeScript" width="40" height="40" />
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mongodb/mongodb-original-wordmark.svg" title="MongoDB" alt="MongoDB" width="40" height="40" />
+            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original-wordmark.svg" title="Python" alt="Python" width="40" height="40" />
+          </div>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.icons_lkd}>
+            <a href="https://www.linkedin.com/in/vreksidler/">
+              <img src="https://img.shields.io/badge/-Linkedin-white?style=flat&logo=Linkedin&logoColor=black" width="150" height="45" alt="Click to Linkedin Profile" />
+            </a>
+          </div>
+          <div className={styles.icons_itg}>
+            <a href="https://www.instagram.com/vitao_reks/">
+              <img src="https://img.shields.io/badge/-Instagram-white?style=flat&logo=Instagram&logoColor=black" width="150" height="45" alt="Click to Linkedin Profile" />
+            </a>
+          </div>
+          <div className={styles.icons_github}>
+            <a href="https://github.com/vbreksidler/">
+              <img src="https://img.shields.io/badge/-Github-white?style=flat&logo=Github&logoColor=black" width="150" height="45" alt="Click to Linkedin Profile" />
+            </a>
+          </div>
+        </div>
         <div id="down"></div>
-        <section className={styles.content}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
-          <p align="center"><a href="https://twitter.com/nodws" className={styles.btn} twtr target="_b">Follow me on Twitter</a>
-          </p>
-        </section>
-      </header>
+      </header >
     );
   }
 }
