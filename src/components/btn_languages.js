1fd25517de7708345acc7b3ebb05b5c8b5c112f5
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-10-26 14:20:56 -0300

    feat: contents

--- src/components/btn_languages.js
+++ src/components/btn_languages.js
@@ -9,11 +9,14 @@ function Btn_Languages() {
     };
     return (
         <div>
-            <button className={styles.btn_languages} onClick={toggleMenu}>Minhas ferramentas</button>
+            <button className={styles.btn_languages} onClick={toggleMenu}>Sobre meu trabalho</button>
             {openMenu ? (
                 <div>
                     <p>
-                        AASDADASDADASD
+                        • Conhecimentos Soft Skills: Proativo, organizado, dinâmico e comunicativo, com habilidade em adaptar-se às novas rotinas e, facilidade no relacionamento interpessoal.
+                    </p>
+                    <p>
+                        • Conhecimentos Hard Skills: HTML, CSS, JavScript, React, Context API, Metodologias Ágeis, Componentes e estados, React Hooks, Higher Order Functions, GitHUb, Testes Unitários, Unix & Bash, Docker, MySQL.
                     </p>
                 </div>
             ) : null}
--- src/components/btn_projects.js
+++ src/components/btn_projects.js
@@ -12,14 +12,20 @@ function Btn_Projects() {
             <button className={styles.btn_languages} onClick={toggleMenu}>Projetos</button>
             {openMenu ? (
                 <div>
-                    <p>
-                        AASDADASDADASD
-                    </p>
+                    <div>
+                        <p>Project Online Store</p>
+                        <img src="https://github.com/vbreksidler/vbreksidler/blob/master/project-online-store-master/project-gif-nonresponsive.gif?raw=true"
+                            alt="Project Online Store" width="1000" />
+                        <img src="https://github.com/DeboraSerra/project-online-store/raw/master/project-front-master-responsive.gif"
+                            alt="Project Online Store" width="1000" />
+                    </div>
+                    <a className={styles.projects_content} href="https://github.com/vbreksidler/vbreksidler">
+                        Clique aqui para acessar meu repositório no Github.
+                    </a>
                 </div>
             ) : null}
         </div>
     );
 }
-
 
 export default Btn_Projects;
