c805eb5c78d8ec10245330194017074d6e3915ea
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-10-27 15:31:58 -0300

    fix font

--- src/components/btn_projects.js
+++ src/components/btn_projects.js
@@ -15,20 +15,20 @@ function Btn_Projects() {
                     <div className={styles.projects_content}>
                         <a href="https://github.com/vbreksidler/project_online_store">
                             Clique aqui para acessar o repositório Project Online Store
+                            <div>
+                                <img src="https://github.com/vbreksidler/project_online_store/blob/main/project-front-master-responsive.gif?raw=true"
+                                    alt="Project Online Store" width="80%" />
+                            </div>
                         </a>
-                        <div>
-                            <img src="https://github.com/vbreksidler/project_online_store/blob/main/project-front-master-responsive.gif?raw=true"
-                                alt="Project Online Store" width="80%" />
-                        </div>
                     </div>
                     <div className={styles.projects_content}>
                         <a href="https://github.com/vbreksidler/project_delivery_app">
                             Clique aqui para acessar o repositório Delivery App
+                            <div>
+                                <img src="https://github.com/vbreksidler/portifoliovbr/blob/master/src/images/output_name.gif?raw=true"
+                                    alt="Project Delivery App" width="80%" />
+                            </div>
                         </a>
-                        <div>
-                            <img src="https://github.com/vbreksidler/portifoliovbr/blob/master/src/images/output_name.gif?raw=true"
-                                alt="Project Delivery App" width="80%" />
-                        </div>
                     </div>
                     <div>
                         <a href="https://github.com/vbreksidler">
