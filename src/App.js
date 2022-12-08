7dc9e4dcfaeb4c058366852235250128d4d161fe
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-12-08 15:44:50 -0300

    new css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Footer from './pages/Footer';
 import Header from './pages/Header';
 import Body from './pages/Body';
+import styles from './components/styles.module.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.App}>
         <Header />
         <Body />
         <Footer />
--- src/pages/Body.js
+++ src/pages/Body.js
@@ -7,25 +7,23 @@ import styles from '../components/styles.module.scss';
 function Body() {
   return (
     <body>
-      <div className={styles.body_}>
-        <section className={styles.content}>
-          <div className={styles.p_content}>
-            <p>
-              Olá, obrigado pela visita! Me chamo Victor, e estou mergulhando nesse universo codificado!
-            </p>
-          </div>
-          <div className={styles.content_btns}>
-            <div id="btn_hello"></div>
-            <div className={styles.all_btns}>
-              <a href="#btn_hello"> {<Btn_HelloWorld />}</a>
-              <div id="btn_lang"></div>
-              <a href="#btn_lang"> {<Btn_my_work />}</a>
-              <div id="btn_projects"></div>
-              <a href="#btn_projects"> {<Btn_Projects />}</a>
-            </div>
-          </div>
-        </section>
+      <div className={styles.p_content}>
+        <p>
+          Olá, obrigado pela visita! Me chamo Victor, e estou mergulhando nesse universo codificado!
+        </p>
       </div>
+      <section className={styles.content}>
+        <div className={styles.content_btns}>
+          <div id="btn_hello"></div>
+          <div className={styles.all_btns}>
+            <a href="#btn_hello"> {<Btn_HelloWorld />}</a>
+            <div id="btn_lang"></div>
+            <a href="#btn_lang"> {<Btn_my_work />}</a>
+            <div id="btn_projects"></div>
+            <a href="#btn_projects"> {<Btn_Projects />}</a>
+          </div>
+        </div>
+      </section>
     </body>
   );
 }
