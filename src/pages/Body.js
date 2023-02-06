3e07075e2b08ff0861004b10105ecc9ae62a85f3
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2023-02-06 15:56:42 -0300

    Atualizando portifolio

--- src/pages/Body.js
+++ src/pages/Body.js
@@ -1,123 +1,93 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from '../components/styles.module.scss';
 
 function Body() {
 
-  const [openMenu, setOpenMenu] = useState(false);
-  const [openMenu1, setOpenMenu1] = useState(false);
-  const [openMenu2, setOpenMenu2] = useState(false);
+  // const [openMenu, setOpenMenu] = useState(false);
+  // const [openMenu1, setOpenMenu1] = useState(false);
+  // const [openMenu2, setOpenMenu2] = useState(false);
 
-  const toggleMenu = () => {
-    setOpenMenu(!openMenu);
-  };
+  // const toggleMenu = () => {
+  //   setOpenMenu(!openMenu);
+  // };
 
-  const toggleMenu1 = () => {
-    setOpenMenu1(!openMenu1);
-  };
+  // const toggleMenu1 = () => {
+  //   setOpenMenu1(!openMenu1);
+  // };
 
-  const toggleMenu2 = () => {
-    setOpenMenu2(!openMenu2);
-  };
+  // const toggleMenu2 = () => {
+  //   setOpenMenu2(!openMenu2);
+  // };
 
 
   return (
     <body>
       <div className={styles.p_content}>
+        <h3>HELLO WORLD</h3>
         <p>
-          Olá, obrigado pela visita! Me chamo Victor, e estou mergulhando nesse universo codificado!
+          Obrigado pela visita! Sou Desenvolvedor de Software e através da tecnologia e inovação, almejo melhorar a vida das pessoas.
+          Sou apaixonado pela metamorfose, pelo novo, pelo desconhecido e claro, pela tecnologia.
         </p>
+      </div>
+      <div className={styles.p_content1}>
+        <h3>• Soft Skills</h3>
+        <p>
+          Proativo, organizado, dinâmico e comunicativo, com habilidade em adaptar-se às novas rotinas e, facilidade no relacionamento interpessoal.
+        </p>
+        <h3>• Hard Skills</h3>
+        <p>
+          HTML, CSS, SASS, Python, JavaScript, TypeScript, Redux, React, Context API, NodeJS, Express, Metodologias Ágeis, Componentes e Estados, React Hooks, Higher Order Functions, GitHub, Unix & Bash, Docker, MySQL, MongoDB, DynamoDB, AWS Cloud.
+        </p>
+        <div className={styles.icons}>
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original-wordmark.svg" title="React" alt="React" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/redux/redux-original.svg" title="Redux" alt="Redux " width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain-wordmark.svg" title="CSS3" alt="CSS" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg" title="HTML5" alt="HTML" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mysql/mysql-original-wordmark.svg" title="MySQL" alt="MySQL" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original-wordmark.svg" title="NodeJS" alt="NodeJS" width="40" height="40" />&nbsp;
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sass/sass-original.svg" title="Sass" alt="Sass" width="40" height="40" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg" title="TypeScript" alt="TypeScript" width="40" height="40" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mongodb/mongodb-original-wordmark.svg" title="MongoDB" alt="MongoDB" width="40" height="40" />
+          <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original-wordmark.svg" title="Python" alt="Python" width="40" height="40" />
+        </div>
+      </div>
+      <section className={styles.section_projects}>
+        <h3>PROJETOS</h3>
+        <div className={styles.container_work}>
+          <div className={styles.carousel}>
+            <div className={styles.carousel__face}><span>1</span></div>
+            <div className={styles.carousel__face}><span>2</span></div>
+            <div className={styles.carousel__face}><span>3</span></div>
+            <div className={styles.carousel__face}><span>4</span></div>
+          </div>
+        </div>
+      </section>
+      <div className={styles.projects_content}>
+        <a href="https://github.com/vbreksidler/project_online_store">
+          1 - Project Online Store
+        </a>
+      </div>
+      <div className={styles.projects_content}>
+        <a href="https://github.com/vbreksidler/project_delivery_app">
+          2 - Delivery App
+        </a>
+      </div>
+      <div className={styles.projects_content}>
+        <a href="https://github.com/vbreksidler/project-patient-register">
+          3 - Patient Register
+        </a>
       </div>
       <section className={styles.content}>
         <div className={styles.content_btns}>
-          <a href="#down1">
+          {/* <a href="#down1">
             <button className={styles.main} onClick={toggleMenu1}>Hello World</button>
-          </a>
-          <a href="#down2">
+          </a> */}
+          {/* <a href="#down2">
             <button className={styles.main} onClick={toggleMenu2}>Sobre meu trabalho</button>
-          </a>
-          <a href="#down3">
-            <button className={styles.main} onClick={toggleMenu}>Projetos</button>
-          </a>
+          </a> */}
         </div>
       </section>
-      <div className={styles.open_content}>
-        <div id="down1"></div>
-        {openMenu1 ? (
-          <div className={styles.content_hello}>
-            <p>HELLO WORLD</p>
-            <p>
-              Acredito no trabalho com excelência, na cooperação, no respeito e na prática da empatia.
-              Sei que sozinho consigo chegar mais rápido ao objetivo, mas em time, posso ir muito além dele!
-            </p>
-            <p>
-              Nasci em Curitiba/PR, desde sempre fui muito criativo, e também sempre tive muita curiosidade
-              sobre tudo ao meu redor, gostava muito de explorar lugares diferentes, observar os animais, desmontar e montar relógios, videogames,
-              controles, etc. Até hoje carrego essa vontade do novo, não atoa, eu passei por diversas áreas antes de chegar aqui, na tecnologia.
-              Trabalhei como auxiliar geral, químico de processo, desenhista mecânico, assistente administrativo em tecnologia e inovação, empreendedor,
-              músico, mecânico, entre varios outros.
-              Meu último emprego foi como gerente geral na empresa QuickUp, empresa familiar do meu irmao, que aluga máquinas
-              de lanches e de café para grandes instituições, como a PIB (primeira igreja batista). Amo trabalhar e amo estudar,
-              nessa minha jornada comecei 3 faculdades, química ambiental na UTFPR , administração na FAE  e a última foi comunicação
-              organizacional na UTFPR.
-            </p>
-            <p>
-              Sempre gostei de várias áreas diferentes, porém a área da tecnologia me abriu os olhos, por ser uma área dinâmica, desafiadora e empolgante, uma área que me desperta a vontade
-              de buscar mais, criar mais, e entender mais. Com a trybe meus horizontes se ampliaram, começamos devagar com os fundamentos, onde aprendi
-              sobre Unix e bash, dominar o github, e a base da programação com HTML e css e lógica de programação. Quando pisquei já estava lidando com
-              a arte do React em frontend, e logo mais, aprendendo sobre Node no backend, foi quando eu realmente percebi o tamanho do universo que existe na
-              programacao. A area da tecnologia me apresentou um caminho diferente aonde traz um balanco mais dinamico entre o desenvolvimento de carreira  e
-              um impacto de valores mais concretos, me sinto sempre motivado e impulsionado a tirar o melhor de mim. Na area, a habilidade de se adaptar e
-              aprender é de essencial valor,  diferente das experiencias passadas onde me sentia mais engessado e ultrapassado.
-            </p>
-            <p>
-              Com resultado disso espero
-              melhorar a vida das pessoas, desde uma ajuda no dia a dia até quem sabe, trabalhar numa empresa que gere impacto no desenvolvimento social.
-              Sou apaixonado pela metamorfose, pelo novo, pelo desconhecido e claro pela tecnologia.
-            </p>
-          </div>
-        ) : null}
-        <div id="down2"></div>
-        {openMenu2 ? (
-          <div className={styles.content_work}>
-            <p>SOBRE MEU TRABALHO</p>
-            <p>
-              • Conhecimentos Soft Skills: Proativo, organizado, dinâmico e comunicativo, com habilidade em adaptar-se às novas rotinas e, facilidade no relacionamento interpessoal.
-            </p>
-            <p>
-              • Conhecimentos Hard Skills: HTML, CSS, JavScript, React, Context API, Metodologias Ágeis, Componentes e estados, React Hooks, Higher Order Functions, GitHUb, Testes Unitários, Unix & Bash, Docker, MySQL.
-            </p>
-          </div>
-        ) : null}
-        <div id="down3"></div>
-        {openMenu ? (
-          <section className={styles.section_projects}>
-            <p>PROJETOS</p>
-            <div className={styles.projects_content}>
-              <a href="https://github.com/vbreksidler/project_online_store">
-                1 Project Online Store
-              </a>
-            </div>
-            <div className={styles.projects_content}>
-              <a href="https://github.com/vbreksidler/project_delivery_app">
-                2 Delivery App
-              </a>
-            </div>
-            <div className={styles.projects_content}>
-              <a href="https://github.com/vbreksidler/project-patient-register">
-                3 Patient Register
-              </a>
-            </div>
-            <div className={styles.container_work}>
-              <div className={styles.carousel}>
-                <div className={styles.carousel__face}><span>1</span></div>
-                <div className={styles.carousel__face}><span>2</span></div>
-                <div className={styles.carousel__face}><span>3</span></div>
-                <div className={styles.carousel__face}><span>4</span></div>
-              </div>
-            </div>
-          </section>
-        ) : null}
-      </div>
     </body>
   );
 }
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,9 +5,28 @@ function Footer() {
     return (
         <section className={styles.footer}>
             <footer className={styles.fixed_footer}>
+                <div className={styles.links_content}>
+                    <div className={styles.links}>
+                        <div className={styles.icons_linkedin}>
+                            <a href="https://www.linkedin.com/in/vreksidler/">
+                                <img src="https://img.shields.io/badge/-Linkedin-blue?style=flat&logo=Linkedin&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
+                            </a>
+                        </div>
+                        <div className={styles.icons_instagram}>
+                            <a href="https://www.instagram.com/vitao_reks/">
+                                <img src="https://img.shields.io/badge/-Instagram-purple?style=flat&logo=Instagram&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
+                            </a>
+                        </div>
+                        <div className={styles.icons_github}>
+                            <a href="https://github.com/vbreksidler/">
+                                <img src="https://img.shields.io/badge/-Github-black?style=flat&logo=Github&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <div class={styles.content_footer}>
                     <div className={styles.icon_content}>
-                        <div className={styles.icons}>
+                        {/* <div className={styles.icons}>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original-wordmark.svg" title="React" alt="React" width="40" height="40" />&nbsp;
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/redux/redux-original.svg" title="Redux" alt="Redux " width="40" height="40" />&nbsp;
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain-wordmark.svg" title="CSS3" alt="CSS" width="40" height="40" />&nbsp;
@@ -19,28 +38,9 @@ function Footer() {
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg" title="TypeScript" alt="TypeScript" width="40" height="40" />
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mongodb/mongodb-original-wordmark.svg" title="MongoDB" alt="MongoDB" width="40" height="40" />
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original-wordmark.svg" title="Python" alt="Python" width="40" height="40" />
-                        </div>
+                        </div> */}
                     </div>
-                    <p className={styles.p_footer}> ©VBR, 2022. Desenvolvido em Javascript, com React, e estilização com Sass.</p>
-                    <div className={styles.links_content}>
-                        <div className={styles.links}>
-                            <div className={styles.icons_linkedin}>
-                                <a href="https://www.linkedin.com/in/vreksidler/">
-                                    <img src="https://img.shields.io/badge/-Linkedin-blue?style=flat&logo=Linkedin&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
-                                </a>
-                            </div>
-                            <div className={styles.icons_instagram}>
-                                <a href="https://www.instagram.com/vitao_reks/">
-                                    <img src="https://img.shields.io/badge/-Instagram-purple?style=flat&logo=Instagram&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
-                                </a>
-                            </div>
-                            <div className={styles.icons_github}>
-                                <a href="https://github.com/vbreksidler/">
-                                    <img src="https://img.shields.io/badge/-Github-black?style=flat&logo=Github&logoColor=white" width="100" height="30" alt="Click to Linkedin Profile" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    <p className={styles.p_footer}> ©VBR, 2023.</p>
                 </div>
             </footer>
         </section>
